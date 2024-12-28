dfb5024336f6c82a4012290e7e708a6ee082070d
Prudence97729 <1559959121@qq.com>
2024-12-28 11:40:55 +0800

    第一次提交

---
+++ zsq/src/main/java/com/zsq/ZsqApplication.java
@@ -0,0 +1,13 @@
+package com.zsq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZsqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZsqApplication.class, args);
+	}
+
+}
